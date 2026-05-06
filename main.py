347802d6e6f86d817917cb0e34c6dacd6a9dd7cc
tading047-alt <tading047@gmail.com>
2026-05-06 13:37:39 +0100

    Refactor Google Sheets credentials loading method
    Updated Google Sheets credentials handling to read from environment variables instead of a file.

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 import pandas as pd
 import gspread
+import os
+import json
 from google.oauth2.service_account import Credentials
+
+# ✅ قراءة credentials من ENV (Railway)
+creds_dict = json.loads(os.environ["GOOGLE_CREDENTIALS"])
 
 SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive"
 ]
 
-creds = Credentials.from_service_account_file(
-    "credentials.json",
-    scopes=SCOPES
-)
+creds = Credentials.from_service_account_info(creds_dict, scopes=SCOPES)
 
 client = gspread.authorize(creds)
 
+# ✅ Google Sheet ID
 sheet = client.open_by_key("1RAWDvovHZZ7mEj9A0soo2XnPOudVadxu8KuZeQaR-dM").sheet1
 
+# ✅ قراءة البيانات
 data = sheet.get_all_records()
+
 df = pd.DataFrame(data)
 
+print("DATA LOADED ✅")
 print(df.head())
